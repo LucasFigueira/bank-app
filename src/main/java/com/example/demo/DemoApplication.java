aeeb1799519c3ff86207524b3a0bb637b3ae8e78
Lucas Figueira <lucas.figueira@ilia.digital>
2020-11-29 12:34:43 -0300

    #deploydev

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("TEST");
+		System.out.println("TEST 7");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
